d5b02d41b087521faa09fa468bdaf259e158c764
Joao Pedro De Munno Durante <joao.durante@venturus.org.br>
2022-06-30 12:28:47 -0300

    implemented GradientBoosting method
    started implementation of a random playlist classification

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os.path
 import pandas as pd
-from spotifyservice import export_track_features_to_csv
+from spotifyservice import SpotifyService
 import constants
 import matplotlib
 matplotlib.use('WebAgg')
@@ -9,19 +9,29 @@ import seaborn as sns
 import pickle
 
 # sklearn imports
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.model_selection import cross_val_score, cross_val_predict, train_test_split, GridSearchCV
+from sklearn.ensemble import GradientBoostingClassifier
 
+# create SpotifyService instance
+spotifyService = SpotifyService()
 
 # check if csv file containing the data already exists, if not, call the function to import it from spotify's API
-def import_data():
+def import_and_process_data():
     csv_exists = os.path.exists(constants.DATASET_FILE_NAME)
     if csv_exists == False:
-        export_track_features_to_csv()
+        spotifyService.export_track_features_to_csv()
 
-    return pd.read_csv(constants.DATASET_FILE_NAME)
+    df = pd.read_csv(constants.DATASET_FILE_NAME)
+    X = df[constants.AUDIO_FEATURES_PROPERTIES]
+    Y = df['mood']
+
+    # preprocessing/normalize our data
+    scaled = MinMaxScaler().fit_transform(X)
+
+    return scaled, Y
 
 # split the dataset into training and test sets (75% training, 25% tests)
 def split_in_train_test_data(X, Y):
@@ -34,40 +44,58 @@ def split_in_train_test_data(X, Y):
     )
 
 # plot the confusion matrix
-def plot_confusion_matrix(testY, predsY, target):
+def plot_confusion_matrix(chartTitle, testY, predsY, target):
     labels = target.drop_duplicates().tolist()
     confusionMatrix = confusion_matrix(testY, predsY, labels=labels)
     ax = plt.subplot()
     sns.heatmap(confusionMatrix,annot=True,ax=ax)
     ax.set_xlabel('Predicted labels')
     ax.set_ylabel('True labels')
-    ax.set_title('Confusion Matrix')
+    ax.set_title(chartTitle)
     ax.xaxis.set_ticklabels(labels)
     ax.yaxis.set_ticklabels(labels)
     plt.show()
 
+
+
 # import the data
-df = import_data()
-X = df[constants.AUDIO_FEATURES_PROPERTIES]
-Y = df['mood']
-
-# preprocessing/normalize our data
-scaled = StandardScaler().fit_transform(X)
-
+X, Y = import_and_process_data()
 # split our data in training and tests sets (75% training, 25% tests)
-trainX, testX, trainY, testY = split_in_train_test_data(scaled, df['mood'])
+trainX, testX, trainY, testY = split_in_train_test_data(X, Y)
+
 
 # instantiate and train using LogisticRegression
 logRegression = LogisticRegression(max_iter=2000)
 logRegression.fit(trainX, trainY)
 predsY = logRegression.predict(testX)
-predsDf = pd.Series(predsY).groupby(predsDf).size().reset_index().values.tolist()
-print(predsDf)
+predsResult = pd.Series(predsY)
+predsResult = predsResult.groupby(predsResult).size().reset_index().values.tolist()
+print('LogisticRegression', accuracy_score(testY, predsY))
+# plot_confusion_matrix('LogisticRegression Confusion Matrix', testY, predsY, Y)
 
-# das 50 musicas happy, avaliou-se 45 happy, 1 aggressive e 4 sads
-# das 50 agressivas, avaliou-se 34 aggressives, 7 sads e 9 calms
-# das 50 sads, 8 happy, 1 aggressive, 39 sads e 2 calms
-# das 43 calms, 6 aggressive, 4 sads e 33 calms
-print(accuracy_score(testY, predsY))
-plot_confusion_matrix(testY, predsY, df['mood'])
+
+# instantiate and train using GradientBoosting
+gradBoosting = GradientBoostingClassifier()
+gradBoosting.fit(trainX, trainY)
+predsY = gradBoosting.predict(testX)
+predsResult = pd.Series(predsY)
+predsResult = predsResult.groupby(predsResult).size().reset_index().values.tolist()
+print('GradientBoosting', accuracy_score(testY, predsY))
+# plot_confusion_matrix('GradientBoosting Confusion Matrix', testY, predsY, df['mood'])
+
+
+
+# userPlaylistId = input("Please provide your playlist url: ")
+# userPlaylistId = 'https://open.spotify.com/playlist/4mfobAcyw052F12K4vwtoW?si=db57e591a0404e67'   # calm
+# userPlaylistId = 'https://open.spotify.com/playlist/0IAG5sPikOCo5nvyKJjCYo?si=4c5019d5032c4a1b'   # happy
+# userPlaylistId = 'https://open.spotify.com/playlist/69SjVFpFkgTIpsuZWGZN6r?si=e9cb0c86cae64847'   # aggressive
+# userPlaylistId = 'https://open.spotify.com/playlist/14qQx1Xfrv874K6GC9kKNd?si=2e667e78be5044b7'   # sad
+
+userTrackFeatures = spotifyService.get_track_feature_from_user_playlist(userPlaylistId)
+userTrackFeaturesList = userTrackFeatures[constants.AUDIO_FEATURES_PROPERTIES].values.tolist()
+scaled = MinMaxScaler().fit_transform(userTrackFeaturesList)
+userPredsY = logRegression.predict(scaled)
+predsResult = pd.Series(userPredsY)
+predsResult = predsResult.groupby(predsResult).size().reset_index().values.tolist()
+print(predsResult)
 
--- spotifyservice.py
+++ spotifyservice.py
@@ -4,75 +4,79 @@ import math
 import json
 import constants
 
+class SpotifyService:
+    def __init__(self):
+        self.instance = self.authenticate_and_instantiate()
 
-def authenticate_and_instantiate():
-    token = spotipy.util.prompt_for_user_token(
-        username='12165639406', 
-        scope='user-library-read',
-        client_id='a5646fd64ee74edabc2781812067f237',
-        client_secret='f2633dc8c0c241c0a93b6688edaabdc6',
-        redirect_uri='http://localhost:5000'
-    )
+    def authenticate_and_instantiate(self):
+        token = spotipy.util.prompt_for_user_token(
+            username='12165639406', 
+            scope='user-library-read',
+            client_id='a5646fd64ee74edabc2781812067f237',
+            client_secret='f2633dc8c0c241c0a93b6688edaabdc6',
+            redirect_uri='http://localhost:5000'
+        )
 
-    return spotipy.Spotify(auth=token, requests_timeout=20)
+        return spotipy.Spotify(auth=token, requests_timeout=20)
 
-def get_track_ids_from_playlist(spotifyInstance, playlist_id):
-    """
-        Get the track ids from a playlist
-        Arguments:
-            spotifyInstance: instance of spotipy.Spotify
-            playlist_id: playlist id
-        Return:
-            List containing the track ids from the playlist received by param
-    """
-    try:
-        return spotifyInstance.playlist_tracks(playlist_id)
-    except:
-        print('failed to get track_ids from playlist: {}'.format(playlist_id))
-    
-def get_tracks_audio_features(spotifyInstance, track_list):
-    """
-        Get the track audio features from a playlist
-        Arguments:
-            spotifyInstance: instance of spotipy.Spotify
-            track_list: list containing the track ids
-        Return:
-            Dataframe containing the track audio features from the playlist received by param
-    """
+    def get_track_ids_from_playlist(self, playlist_url):
+        """
+            Get the track ids from a playlist
+            Arguments:
+                playlist_id: playlist id
+            Return:
+                List containing the track ids from the playlist received by param
+        """
+        try:
+            playlist_id = playlist_url[34:56]
+            tracks = self.instance.playlist_tracks(playlist_id)
+            return [i['track']['id'] for i in tracks['items']]
+        except:
+            print('failed to get track_ids from playlist: {}'.format(playlist_id))
 
-    request_size = 50                                               # number of tracks per request
-    request_num = int(math.ceil(len(track_list) / request_size))    # number of requests
-    track_features_list = []
+    def get_tracks_audio_features(self, track_list):
+        """
+            Get the track audio features from a playlist
+            Arguments:
+                track_list: list containing the track ids
+            Return:
+                Dataframe containing the track audio features from the playlist received by param
+        """
 
-    for i in range(request_num):
-        initial_index = i * request_size
-        final_index = min(((i + 1) * request_size), len(track_list))
-        request_ids = track_list[initial_index : final_index]
-        features = spotifyInstance.audio_features(tracks=request_ids)
-        track_features_list += features
-    
-    track_features_df = pd.DataFrame(track_features_list)
-    return track_features_df[constants.AUDIO_FEATURES_PROPERTIES]
+        request_size = 50                                               # number of tracks per request
+        request_num = int(math.ceil(len(track_list) / request_size))    # number of requests
+        track_features_list = []
 
-def export_track_features_to_csv():
-    """
-        Export the audio features from the playlists to a csv file
-    """
-    df = pd.DataFrame()
-    spotifyInstance = authenticate_and_instantiate()
-    file = open(constants.PLAYLISTS_FILE_PATH)
-    playlists = json.load(file)
+        for i in range(request_num):
+            initial_index = i * request_size
+            final_index = min(((i + 1) * request_size), len(track_list))
+            request_ids = track_list[initial_index : final_index]
+            features = self.instance.audio_features(tracks=request_ids)
+            track_features_list += features
+        
+        track_features_df = pd.DataFrame(track_features_list)
+        return track_features_df[constants.AUDIO_FEATURES_PROPERTIES]
 
-    for mood, urls in playlists.items():
-        for url in urls:
-            playlist_id = url[34:56]
-            tracks = get_track_ids_from_playlist(spotifyInstance, playlist_id)
-            track_ids = [i['track']['id'] for i in tracks['items']]
-            features = get_tracks_audio_features(spotifyInstance, track_ids)
-            features['id'] = track_ids
-            features['mood'] = mood
-            df = pd.concat([df, features])
-    
-    df.to_csv(constants.DATASET_FILE_NAME)
+    def get_track_feature_from_user_playlist(self, playlistUrl):
+        track_ids = self.get_track_ids_from_playlist(playlistUrl)
+        features = self.get_tracks_audio_features(track_ids)
+        features['id'] = track_ids
+        return pd.DataFrame(features)
 
-export_track_features_to_csv()
+    def export_track_features_to_csv(self):
+        """
+            Export the audio features from the playlists to a csv file
+        """
+        df = pd.DataFrame()
+        file = open(constants.PLAYLISTS_FILE_PATH)
+        playlists = json.load(file)
+
+        for mood, urls in playlists.items():
+            for url in urls:
+                track_ids = self.get_track_ids_from_playlist(url)
+                features = self.get_tracks_audio_features(track_ids)
+                features['id'] = track_ids
+                features['mood'] = mood
+                df = pd.concat([df, features])
+        
+        df.to_csv(constants.DATASET_FILE_NAME)
